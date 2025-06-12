860b28e6d16d4f380075e948c2b5c6523ab0ffad
Diones <pereiradiones987@gmail.com>
2025-06-11 22:42:33 -0300

    query de categorias

--- frontend/src/utils/queries.ts
+++ frontend/src/utils/queries.ts
@@ -1,10 +1,20 @@
-import { getAllSpents } from "@/service/api";
+import { getAllCategories, getAllSpents } from "@/service/api";
 import { useQuery } from "@tanstack/react-query"
 
 export const useAllSpents = (page: number, pageSize: number) => {
   const query = useQuery({
     queryKey: ['all-spents', { page, pageSize }],
     queryFn: () => getAllSpents(page, pageSize),
+  });
+
+  return query;
+}
+
+
+export const useCategories = () => {
+  const query = useQuery({
+    queryKey: ['categories'],
+    queryFn: getAllCategories,
   });
 
   return query;
