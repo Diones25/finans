a076f54cc88f759f93e470a328eeb4ba7c942b54
Diones <pereiradiones987@gmail.com>
2025-06-11 22:49:51 -0300

    atualizando query de spents

--- frontend/src/utils/queries.ts
+++ frontend/src/utils/queries.ts
@@ -1,10 +1,11 @@
 import { getAllCategories, getAllSpents } from "@/service/api";
-import { useQuery } from "@tanstack/react-query"
+import { keepPreviousData, useQuery } from "@tanstack/react-query"
 
 export const useAllSpents = (page: number, pageSize: number) => {
   const query = useQuery({
     queryKey: ['all-spents', { page, pageSize }],
     queryFn: () => getAllSpents(page, pageSize),
+    placeholderData: keepPreviousData
   });
 
   return query;
