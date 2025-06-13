5a18b2d751795ffff8f819c3e116b9a8719cec4b
Diones <pereiradiones987@gmail.com>
2025-06-13 14:12:09 -0300

    atualizando página de adicionar saldo

--- frontend/src/components/AddBalance.tsx
+++ frontend/src/components/AddBalance.tsx
@@ -1,31 +1,39 @@
-import React, { FormEvent, useState } from 'react';
-import InputCompoment from './InputCompoment';
 import { Button } from './ui/button';
 import { useNavigate, useParams } from "react-router-dom";
-import { addBalanceCategory } from '@/service/api';
+import { useAddBalanceCategory } from '@/utils/mutations';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { addBalanceCategorySchema } from '@/schemas/addBalanceCategorySchema';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Label } from '@radix-ui/react-label';
+import { Input } from './ui/input';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function AddBalance() {
   const navigate = useNavigate();
   const { id } = useParams();
-  const [formData, setFormData] = useState({
-    balance: ''
-  });
+  const addBalanceCategory = useAddBalanceCategory();
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-    setFormData({
-      ...formData,
-      [name]: value
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<z.infer<typeof addBalanceCategorySchema>>({
+    resolver: zodResolver(addBalanceCategorySchema)
+  })
+
+  const handleFormSubmit: SubmitHandler<z.infer<typeof addBalanceCategorySchema>> = (data) => {
+    if (!id) return;
+
+    addBalanceCategory.mutate(
+      { id, balance: data.balance },{
+      onSuccess: () => {
+        //Reseta o formulário
+        reset();
+      },
     });
-  }
-  
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-
-    (async () => {
-      await addBalanceCategory(id, Number(formData.balance));
-      navigate("/");
-    })();
   }
 
   return (
@@ -33,23 +41,25 @@ function AddBalance() {
       <div className='container'>
         <div className="w-[730px]">
           <h1 className="text-3xl font-semibold text-gray-800 my-3">Adicionar saldo</h1>
-          <form onSubmit={handleSubmit}>
-            <InputCompoment
-              label="Saldo"
-              htmlFor="balance"
-              type='text'
-              id="balance"
-              name='balance'
-              value={formData.balance}
-              onChange={handleChange}
-            />
+          <form onSubmit={handleSubmit(handleFormSubmit)}>
+            <div className="mb-2 space-y-2">
+              <Label htmlFor="description">Saldo</Label>
+              <Input
+                id="description"
+                {...register('balance', { valueAsNumber: true })}
+                className={`border ${errors.balance ? 'border-red-600' : 'border-black'} text-black focus:outline-none`}
+              />
+              {errors.balance &&
+                <p className='text-red-600 text-sm'>{errors.balance.message}</p>
+              }
+            </div>
 
             <div className="text-white pt-2 space-x-2 flex justify-end">
               <Button onClick={() => navigate("/")} className="bg-blue-600 hover:bg-blue-600">Voltar</Button>
               <Button type="submit" className="bg-green-600 hover:bg-green-600">Adicionar</Button>
             </div>
           </form>
-
+          <ToastContainer position="bottom-right" autoClose={3000} />
         </div>
       </div> 
     </>
