028914c73c601c12620d13808a36198c4ef88ec1
Diones <pereiradiones987@gmail.com>
2025-06-13 15:08:03 -0300

    atualizando formulario de editar categoria

--- frontend/src/components/EditCategory.tsx
+++ frontend/src/components/EditCategory.tsx
@@ -1,30 +1,52 @@
-import { FormEvent, useEffect, useState } from "react";
-import InputCompoment from "./InputCompoment";
 import { Button } from "./ui/button";
 import { useParams, useNavigate } from "react-router-dom";
-import { editCategory, getOneCategory } from "@/service/api";
+import { useEditCategory } from "@/utils/mutations";
+import { useCategory } from "@/utils/queries";
+import { editCategorySchema } from "@/schemas/editCategorySchema";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Label } from "@radix-ui/react-label";
+import { Input } from "./ui/input";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useEffect } from "react";
 
 function EditCategory() {
-  const { id } = useParams();
   const navigate = useNavigate();
-  const [name, setName] = useState('');
-  const [balance, setBalance] = useState('');
+  const { id } = useParams();
+  const category = useCategory(id as string);
+  const aditCategory = useEditCategory();
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<z.infer<typeof editCategorySchema>>({
+    resolver: zodResolver(editCategorySchema)
+  })
 
   useEffect(() => {
-    (async () => {
-      const res = await getOneCategory(id);
-      setName(res.name);
-      setBalance(res.balance);
-    })();
-  }, [id]);
+    if (category.data) {
+      reset({
+        name: category.data.name,
+        balance: category.data.balance,
+      });
+    }
+  }, [category.data, reset]);
+  
 
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
+  const handleFormSubmit: SubmitHandler<z.infer<typeof editCategorySchema>> = (data) => {
+    if (!id) return;
 
-    (async () => {
-      await editCategory(id, name, Number(balance));
-      navigate("/");
-    })()
+    aditCategory.mutate(
+      { id, data }, {
+      onSuccess: () => {
+        //Reseta o formulário
+        reset();
+      },
+    });
   }
 
   return (
@@ -32,31 +54,37 @@ function EditCategory() {
       <div className='container'>
         <div className="w-[730px]">
           <h1 className="text-3xl font-semibold text-gray-800 my-3">Editar categoria</h1>
-          <form onSubmit={handleSubmit}>
-            <InputCompoment
-              label="Nome"
-              htmlFor="name"
-              type="text"
-              id="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
+          <form onSubmit={handleSubmit(handleFormSubmit)}>
+            <div className="mb-2 space-y-2">
+              <Label htmlFor="description">Nome</Label>
+              <Input
+                id="description"
+                {...register('name')}
+                className={`border ${errors.name ? 'border-red-600' : 'border-black'} text-black focus:outline-none`}
+              />
+              {errors.name &&
+                <p className='text-red-600 text-sm'>{errors.name.message}</p>
+              }
+            </div>
 
-            <InputCompoment
-              label="Saldo"
-              htmlFor="balance"
-              type="text"
-              id="balance"
-              value={balance}
-              onChange={(e) => setBalance(e.target.value)}
-            />
+            <div className="mb-2 space-y-2">
+              <Label htmlFor="description">Saldo</Label>
+              <Input
+                id="description"
+                {...register('balance', { valueAsNumber: true })}
+                className={`border ${errors.balance ? 'border-red-600' : 'border-black'} text-black focus:outline-none`}
+              />
+              {errors.balance &&
+                <p className='text-red-600 text-sm'>{errors.balance.message}</p>
+              }
+            </div>
 
             <div className="text-white pt-2 space-x-2 flex justify-end">
-              <Button type="submit" className="bg-blue-600 hover:bg-blue-600">Voltar</Button>
+              <Button onClick={() => navigate("/")} className="bg-blue-600 hover:bg-blue-600">Voltar</Button>
               <Button type="submit" className="bg-green-600 hover:bg-green-600">Atualizar</Button>
             </div>
           </form>
-
+          <ToastContainer position="bottom-right" autoClose={3000} />
         </div>
       </div>  
     </>
