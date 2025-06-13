6bf0ed20b7a589b49de8ba49dc6fc50c35ad85c7
Diones <pereiradiones987@gmail.com>
2025-06-12 23:33:49 -0300

    atualizando mutation

--- frontend/src/utils/mutations.ts
+++ frontend/src/utils/mutations.ts
@@ -1,15 +1,22 @@
 import { addSpent } from "@/service/api";
 import { useMutation } from "@tanstack/react-query"
 import { queryClient } from "./queryClient";
+import { toast } from 'react-toastify';
 
 export const useAddSpent = () => {
-  const mutation = useMutation({
+  const { mutate: mutation, reset } = useMutation({
     mutationFn: addSpent,
     onSuccess: () => {
       queryClient.invalidateQueries({
         queryKey: ['all-spents'],
       });
-    }
+      //Reseta o formulário
+      reset();
+      toast.success('Gasto cadastrado com sucesso!');
+    },
+    onError: (error) => {
+      toast.error(`Erro ao cadastrar gasto: ${error.message}`);
+    },
   });
 
   return mutation;
