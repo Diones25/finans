a25b6578b17f253fda8bd91c017820d2862a380c
Diones <pereiradiones987@gmail.com>
2025-06-13 15:07:37 -0300

    schema para atualizar categoria

---
+++ frontend/src/schemas/editCategorySchema.ts
@@ -0,0 +1,6 @@
+import { z } from "zod";
+
+export const editCategorySchema = z.object({
+  name: z.string({ message: "Deve ser uma string" }).nonempty("Name é obrigatória").min(4, { message: "Deve ter pelo menos 4 caracteres" }),
+  balance: z.number({ message: "Saldo deve ser um número" }).min(1, { message: "Saldo deve ter pelo menos um caractere" }),
+});
