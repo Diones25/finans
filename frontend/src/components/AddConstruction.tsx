2fb9d3495a891a9f9a47713c51cbcf435946115f
Diones <pereiradiones987@gmail.com>
2025-06-16 20:27:33 -0300

    Atualizando Componente AddConstruction

--- frontend/src/components/AddConstruction.tsx
+++ frontend/src/components/AddConstruction.tsx
@@ -1,32 +1,44 @@
-import React, { FormEvent, useState } from 'react'
-import InputCompoment from './InputCompoment'
+import { useState } from 'react'
 import { Button } from './ui/button'
 import { useNavigate } from 'react-router-dom';
-import { addConstruction } from '@/service/api';
+import { useAddConstruction } from '@/utils/mutations';
+import { addConstructionSchema } from '@/schemas/addConstructionSchema';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Label } from '@radix-ui/react-label';
+import { Input } from './ui/input';
+import { NumericFormat } from 'react-number-format';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function AddConstruction() {
+  const [formattedValue, setFormattedValue] = useState("");
   const navigate = useNavigate();
-  const [formData, setFormData] = useState({
-    name: '',
-    quantity: '',
-    unitaryValue: ''
-  });
+  const addConstruction = useAddConstruction();
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-    setFormData({
-      ...formData,
-      [name]: value
-    });
-  }
+  const {
+    register,
+    handleSubmit,
+    reset,
+    setValue,
+    formState: { errors },
+  } = useForm<z.infer<typeof addConstructionSchema>>({
+    resolver: zodResolver(addConstructionSchema)
+  })
 
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
+  const handleFormSubmit: SubmitHandler<z.infer<typeof addConstructionSchema>> = (data) => {
+    const convertedValue = parseFloat(formattedValue.replace(',', '.'));
 
-    (async () => {
-      await addConstruction(formData.name, Number(formData.quantity), Number(formData.unitaryValue));
-      navigate("/construction");
-    })();
+    addConstruction.mutate(
+      { ...data, unitaryValue: convertedValue },
+      {
+        onSuccess: () => {
+          //Reseta o formulário
+          reset();
+          setFormattedValue("");
+        },
+      });
   }
   
   return (
@@ -34,43 +46,59 @@ function AddConstruction() {
       <div className='container'>
         <div className="w-[730px]">
           <h1 className="text-3xl font-semibold text-gray-800 my-3">Adicionar gasto de construção</h1>
-          <form onSubmit={handleSubmit}>
-            <InputCompoment
-              label="Nome"
-              htmlFor="name"
-              type="text"
-              id="name"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-            />
+          <form onSubmit={handleSubmit(handleFormSubmit)}>
+            <div className="mb-2 space-y-2">
+              <Label htmlFor="name">Nome</Label>
+              <Input
+                id="name"
+                {...register('name')}
+                className={`border ${errors.name ? 'border-red-600' : 'border-black'} text-black focus:outline-none`}
+              />
+              {errors.name &&
+                <p className='text-red-600 text-sm'>{errors.name.message}</p>
+              }
+            </div>
 
-            <InputCompoment
-              label="Quantidade"
-              htmlFor="quantity"
-              type="number"
-              id="quantity"
-              name="quantity"
-              value={formData.quantity}
-              onChange={handleChange}
-            />
+            <div className="mb-2 space-y-2">
+              <Label htmlFor="name">Quantidade</Label>
+              <Input
+                id="quantity"
+                {...register('quantity', { valueAsNumber: true, })}
+                className={`border ${errors.quantity ? 'border-red-600' : 'border-black'} text-black focus:outline-none`}
+              />
+              {errors.quantity &&
+                <p className='text-red-600 text-sm'>{errors.quantity.message}</p>
+              }
+            </div>
 
-            <InputCompoment
-              label="Valor Unitário"
-              htmlFor="unitaryValue"
-              type="number"
-              id="unitaryValue"
-              name="unitaryValue"
-              value={formData.unitaryValue}
-              onChange={handleChange}
-            />
+            <div className="mb-2 space-y-2">
+              <Label htmlFor="unitaryValue">Valor Unitário</Label>
+              <NumericFormat
+                id="unitaryValue"
+                value={formattedValue}
+                decimalSeparator=","
+                thousandSeparator="."
+                allowNegative={false}
+                decimalScale={2}
+                fixedDecimalScale
+                customInput={Input}
+                onValueChange={(values) => {
+                  setFormattedValue(values.value);
+                  setValue("unitaryValue", parseFloat(values.value.replace(',', '.')));
+                }}
+                className={`border ${errors.unitaryValue ? 'border-red-600' : 'border-black'} text-black focus:outline-none`}
+              />
+              {errors.unitaryValue &&
+                <p className='text-red-600 text-sm'>{errors.unitaryValue.message}</p>
+              }
+            </div>
 
             <div className="text-white pt-2 space-x-2 flex justify-end">
-              <Button className="bg-blue-600 hover:bg-blue-600">Voltar</Button>
+              <Button onClick={() => navigate("/construction")} className="bg-blue-600 hover:bg-blue-600">Voltar</Button>
               <Button type="submit" className="bg-green-600 hover:bg-green-600">Cadastrar</Button>
             </div>
           </form>
-
+          <ToastContainer position="bottom-right" autoClose={3000} />
         </div>
       </div>
     </>
