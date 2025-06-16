53e5be6921a9d6cac76dd79e07e313511961898a
Diones <pereiradiones987@gmail.com>
2025-06-16 20:15:11 -0300

    schema addConstructionSchema

---
+++ frontend/src/schemas/addConstructionSchema.ts
@@ -0,0 +1,7 @@
+import { z } from "zod";
+
+export const addConstructionSchema = z.object({
+  name: z.string({ message: "Deve ser uma string" }).nonempty("Name é obrigatória").min(5, { message: "Deve ter pelo menos 5 caracteres" }),
+  quantity: z.number({ message: "Quantidade deve ser um número" }).min(1, { message: "Quantidade deve ter pelo menos um caractere" }),
+  unitaryValue: z.number({ message: "Valor unitário deve ser um número" }).min(1, { message: "Valor unitário deve ter pelo menos um caractere" }),
+});
