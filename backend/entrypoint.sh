89add8af1ae0d2a5404b57a0a7bf64a478e7db8c
Diones <pereiradiones987@gmail.com>
2026-04-07 15:55:03 -0300

    feat: configurações para criação do container backend

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo "Instalando dependências..."
+npm install
+
 echo "Aguardando o banco de dados em db:5432..."
 while ! nc -z db 5432; do
   sleep 1
@@ -12,6 +15,6 @@ echo "Migrações aplicadas! Fazendo o build do projeto..."
 npm run build
 
 echo "Iniciando o servidor..."
-npm start
+npm run start
 
 echo "Servidor iniciado!"
