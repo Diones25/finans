cd845461fdb6f8fa3605430f340a1b45a386c1e0
Diones <pereiradiones987@gmail.com>
2026-07-19 12:53:08 -0300

    fix: removendo caracteres especiais no nome da categoria

--- backend/src/category/dto/create-category.dto.ts
+++ backend/src/category/dto/create-category.dto.ts
@@ -1,11 +1,12 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNumber, IsString, Min, MinLength } from "class-validator";
+import { IsNumber, IsString, Matches, Min, MinLength } from "class-validator";
 
 export class CreateCategoryDto {
 
   @ApiProperty({ example: 'Alimentação', description: 'Nome da categoria', minLength: 4 })
   @IsString({ message: "Nome deve ser uma string" })
   @MinLength(4, { message: "Nome deve ter no mínimo 4 caracteres" })
+  @Matches(/^[A-Za-zÀ-ÿ0-9\s]+$/, { message: "Nome deve conter apenas letras, números e espaços" })
   name: string;
 
   @ApiProperty({ example: 100, description: 'Saldo da categoria', minLength: 1 })
